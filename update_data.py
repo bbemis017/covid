c5343d182850aaba0f416a579990d501b68fcdca
Ben Bemis <bbemis@alumni.purdue.edu>
2020-04-03 22:21:10 -0500

    Add scraping template

--- update_data.py
+++ update_data.py
@@ -6,9 +6,10 @@ import json
 
 config = None
 
+
 def main():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--gen_template', action='store_true')
+    parser.add_argument('--gen_template')
     parser.add_argument('--download')
 
     args = parser.parse_args()
@@ -16,7 +17,13 @@ def main():
     load_config()
 
     if args.gen_template:
+        template_file = args.gen_template
         print('gen template')
+        template_id = create_template(template_file)
+        print('created template', template_id)
+        if template_id is not None:
+            config['template_id'] = template_id
+            write_dict_to_json(config, 'config.json')
 
     if args.download:
         download_date = args.download
@@ -25,18 +32,30 @@ def main():
         df = clean_data(raw_data, download_date)
         save_data(df)
 
+
 def load_config():
 
     global config
-    with open('config.json', 'r') as file:
-        config = json.load(file)
+    config = load_dict_from_json('config.json')
+
+
+def load_dict_from_json(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+
+def write_dict_to_json(dict, filename):
+    with open(filename, 'w') as file:
+        file.write(json.dumps(dict, indent=4))
+
 
 def get_headers():
     return {'Authorization': 'KEY {key}'.format(key=config['api_key'])}
 
+
 def start_job(template_id):
     print('start job')
-    url = '{host}/job/{template}'.format(host=config['api_host'],template=template_id)
+    url = '{host}/job/{template}'.format(host=config['api_host'], template=template_id)
     headers = get_headers()
 
     r = requests.post(url=url, headers=headers, data={})
@@ -44,6 +63,7 @@ def start_job(template_id):
         return r.json()['id']
     else:
         return None
+
 
 def get_status(job_id):
     url = '{host}/job/{id}/status'.format(host=config['api_host'], id=job_id)
@@ -55,6 +75,23 @@ def get_status(job_id):
             return data['data']
         else:
             return None
+
+
+def create_template(template_file):
+
+    template = load_dict_from_json(template_file)
+
+    url = '{host}/template'.format(host=config['api_host'])
+
+    r = requests.post(url=url, headers=get_headers(), json=template)
+    data = r.json()
+    if 'template_id' in data:
+        return data['template_id']
+    else:
+        print('Template Error')
+        print(data)
+        return None
+
 
 def get_data(template_id):
 
@@ -68,24 +105,27 @@ def get_data(template_id):
 
     return data
 
+
 def remove_extras(df, column):
     df[column] = df[column].str.replace(',', '')
     df[column] = df[column].str.replace('+', '')
     df[column] = pd.to_numeric(df[column]).fillna(0).astype(int)
     return df[column]
 
+
 def clean_data(data_dict, date):
 
-   df = pd.DataFrame(data_dict['States'])
+    df = pd.DataFrame(data_dict['States'])
 
-   df['Active Cases'] = remove_extras(df, 'Active Cases')
-   df['New Cases'] = remove_extras(df, 'New Cases')
-   df['New Deaths'] = remove_extras(df, 'New Deaths')
-   df['Total Cases'] = remove_extras(df, 'Total Cases')
-   df['Total Deaths'] = remove_extras(df, 'Total Deaths')
+    df['Active Cases'] = remove_extras(df, 'Active Cases')
+    df['New Cases'] = remove_extras(df, 'New Cases')
+    df['New Deaths'] = remove_extras(df, 'New Deaths')
+    df['Total Cases'] = remove_extras(df, 'Total Cases')
+    df['Total Deaths'] = remove_extras(df, 'Total Deaths')
 
-   df.insert(0, 'Date', date)
-   return df
+    df.insert(0, 'Date', date)
+    return df
+
 
 def save_data(df):
     filename = 'data/worldometer.csv'
@@ -93,8 +133,6 @@ def save_data(df):
 
     new_df = pd.concat([old_df, df])
     new_df.to_csv(filename, index=False)
-
-
 
 
 if __name__ == "__main__":
