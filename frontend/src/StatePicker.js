e191b9e562186f499366dcf766f7c8f118870a1f
Benjamin Bemis <bbemis@alumni.purdue.edu>
2020-04-05 15:32:02 -0500

    added redux

--- frontend/src/StatePicker.js
+++ frontend/src/StatePicker.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import covidData from './worldometer'
+import { connect } from 'react-redux';
 
 class StatePicker extends React.Component {
-    constructor() {
-        super();
-        this.state = this.get_state_map();
-    }
-
-    get_state_map() {
-        let states = {};
-        for(let i = 0; i < covidData.length; i++) {
-            let state_name = covidData[i]['State'];
-            if(Object.values(states).indexOf(state_name) < 0){
-                states[state_name] = false;
-            }
-        }
-        console.log(states);
-        return states;
-    }
-
-    handleClick(name) {
-        this.setState({
-            [name]: !this.state[name]
-        });
-    }
 
 	render() {
 		return (
 			<div className="state-picker col-3 list-group">
                 <h1>States</h1>
                 <div className="items">
-                    {Object.keys(this.state).map((name) =>
+                    {Object.keys(this.props.selected_states).map((name) =>
                         <li
-                            className={`list-group-item ${this.state[name] ? 'selected': ''}`}
-                            onClick={ () => this.handleClick(name)}
+                            className={`list-group-item ${this.props.selected_states[name] ? 'selected': ''}`}
+                            onClick={ () => this.props.dispatch({type: "TOGGLE_STATE", name: name})}
                             >
                                 {name}
                         </li>
@@ -44,6 +22,11 @@ class StatePicker extends React.Component {
 	}
 }
 
+function mapStateToProps(state) {
+    return {
+      selected_states: state.selected_states
+    };
+  }
 
-export default StatePicker
+  export default connect(mapStateToProps)(StatePicker);
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import covidData from './worldometer';
+import _ from 'lodash';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function get_state_map() {
+  let states = {};
+  for(let i = 0; i < covidData.length; i++) {
+      let state_name = covidData[i]['State'];
+      if(Object.values(states).indexOf(state_name) < 0){
+          states[state_name] = false;
+      }
+  }
+  return states;
+}
+
+const initialState = {
+    selected_states: get_state_map()
+};
+
+function reducer(state = initialState, action) {
+  console.log('reducer', state, action);
+
+  switch(action.type) {
+    case 'TOGGLE_STATE':
+      let selected = _.clone(state.selected_states, true);
+      selected[action.name] = !selected[action.name];
+      return {
+        ...state,
+        selected_states: selected
+      }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.dispatch({ type: "INCREMENT" });
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
